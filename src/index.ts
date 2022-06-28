53b3ef30ce4c24b7067a93b690be1550b72deb49
AlexandreCR <devalexandrecr@gmail.com>
2022-06-28 16:26:24 -0500

    refactor: separate routes in files

--- src/index.ts
+++ src/index.ts
@@ -1,66 +1,11 @@
 import * as functions from 'firebase-functions'
-import {Express, Request, Response} from 'express'
-import {validationResult} from 'express-validator'
-import FBAuth from './services/firebase/FBAuth'
-import {UserType} from './services/Types/UserType'
+import {Express} from 'express'
 import * as cors from 'cors'
 import * as express from 'express'
-import {enableValidator} from './validators/users/EnableValidator'
-import {createValidator} from './validators/users/CreateValidator'
+import {authRouter} from './routes/auth/controller'
 
-const app: Express = express()
-app.use(cors({origin: true}))
+const api: Express = express()
+api.use(cors({origin: true}))
+api.use('/auth', authRouter)
 
-app.post('/create-user', createValidator, async (req: Request, res: Response) => {
-  functions.logger.info(`create user ${req.body}`, {structuredData: true})
-  const errors = validationResult(req)
-
-  if (!errors.isEmpty()) {
-    return res.status(400).json({
-      status: 'ERROR',
-      data: errors.array(),
-    })
-  }
-
-  await FBAuth.createUser(req.body as UserType).then((user) => {
-    return res.status(200).json({
-      status: 'OK',
-      data: user.toJSON(),
-    })
-  }).catch((e) => {
-    return res.status(500).json({
-      status: 'FAILED',
-      data: e.message,
-    })
-  })
-
-  return res.end()
-})
-
-app.post('/enable-user', enableValidator, async (req: Request, res: Response) => {
-  functions.logger.info(`enable user ${req.body}`, {structuredData: true})
-  const errors = validationResult(req)
-
-  if (!errors.isEmpty()) {
-    return res.status(400).json({
-      status: 'ERROR',
-      data: errors.array(),
-    })
-  }
-
-  await FBAuth.updateUser(req.body.uid, {disabled: req.body.disabled}).then((user) => {
-    return res.status(200).json({
-      status: 'OK',
-      data: user.toJSON(),
-    })
-  }).catch((e) => {
-    return res.status(500).json({
-      status: 'FAILED',
-      data: e.message,
-    })
-  })
-
-  return res.end()
-})
-
-exports.app = functions.https.onRequest(app)
+exports.api = functions.https.onRequest(api)
---
+++ src/routes/auth/controller.ts
@@ -0,0 +1,63 @@
+import {Request, Response, Router} from 'express'
+import * as functions from 'firebase-functions'
+import {validationResult} from 'express-validator'
+import FBAuth from '../../services/firebase/FBAuth'
+import {UserType} from '../../services/Types/UserType'
+import {createValidator} from './validators/users/CreateValidator'
+import {enableValidator} from './validators/users/EnableValidator'
+
+const controller = Router()
+
+controller.post('/create-user', createValidator, async (req: Request, res: Response) => {
+  functions.logger.info(`create user ${req.body}`, {structuredData: true})
+  const errors = validationResult(req)
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      status: 'ERROR',
+      data: errors.array(),
+    })
+  }
+
+  await FBAuth.createUser(req.body as UserType).then((user) => {
+    return res.status(200).json({
+      status: 'OK',
+      data: user.toJSON(),
+    })
+  }).catch((e) => {
+    return res.status(500).json({
+      status: 'FAILED',
+      data: e.message,
+    })
+  })
+
+  return res.end()
+})
+
+controller.post('/enable-user', enableValidator, async (req: Request, res: Response) => {
+  functions.logger.info(`enable user ${req.body}`, {structuredData: true})
+  const errors = validationResult(req)
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      status: 'ERROR',
+      data: errors.array(),
+    })
+  }
+
+  await FBAuth.updateUser(req.body.uid, {disabled: req.body.disabled}).then((user) => {
+    return res.status(200).json({
+      status: 'OK',
+      data: user.toJSON(),
+    })
+  }).catch((e) => {
+    return res.status(500).json({
+      status: 'FAILED',
+      data: e.message,
+    })
+  })
+
+  return res.end()
+})
+
+export const authRouter = controller
