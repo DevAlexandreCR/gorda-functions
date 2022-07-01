55166c68a050f5d088286c2ffb321b5e44b2d560
AlexandreCR <devalexandrecr@gmail.com>
2022-06-30 22:16:05 -0500

    fix: not assign when service is canceled

--- src/routes/services/controller.ts
+++ src/routes/services/controller.ts
@@ -3,15 +3,26 @@ import FBDatabase from '../../services/firebase/FBDatabase'
 import {Applicant} from './Applicant'
 
 export const assign = functions.database.ref('services/{serviceID}/applicants').onCreate(async (snapshot, context) => {
+  let canceled = false
   const serviceId = context.params.serviceID
   const applicants = new Array<Applicant>()
   const refApplicants = FBDatabase.dbServices().child(serviceId).child('applicants').ref
+  const refStatus = FBDatabase.dbServices().child(serviceId).child('status').ref
   const refService = FBDatabase.dbServices().child(serviceId).ref
+
   refApplicants.on('child_added', (dataSnapshot) => {
     const applicant = dataSnapshot.val() as Applicant
     applicant.id = dataSnapshot.key?? ''
     applicants.push(applicant)
     applicants.sort((a, b) => a.time - b.time)
+  })
+
+  refStatus.on('child_changed', (statusSnapshot) => {
+    const status = statusSnapshot.val() as string
+    if (status === 'canceled') {
+      refApplicants.off()
+      canceled = true
+    }
   })
 
   refApplicants.on('child_removed', (dataSnapshot) => {
@@ -22,14 +33,18 @@ export const assign = functions.database.ref('services/{serviceID}/applicants').
 
   setTimeout(() => {
     refApplicants.off()
-    if (applicants.length > 0) {
+    refService.off()
+    refStatus.off()
+    if (!canceled && applicants.length > 0) {
       const applicant = applicants.shift()
       refService.update({
         status: 'in_progress',
         driver_id: applicant?.id,
       }).then(() => {
+        refService.off()
         console.log(`service ${serviceId} assigned to ${applicant?.id}`)
       }).catch((e) => {
+        refService.off()
         console.log(`error applying service ${serviceId} to driver ${applicant?.id}`, e.message)
         console.table(applicants)
         refService.child('applicants').remove()
