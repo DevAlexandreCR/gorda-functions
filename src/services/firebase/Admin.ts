718dd26223dc82280f6e08a245910d334f3f9dcd
AlexandreCR <devalexandrecr@gmail.com>
2022-07-03 23:41:47 -0500

    support: update functions

--- src/services/firebase/Admin.ts
+++ src/services/firebase/Admin.ts
@@ -1,4 +1,4 @@
-import {initializeApp, App, ServiceAccount, cert} from 'firebase-admin/app'
+import {initializeApp, App, applicationDefault} from 'firebase-admin/app'
 import {getDatabase} from 'firebase-admin/database'
 
 export default class Admin {
@@ -6,13 +6,12 @@ export default class Admin {
 
   public static getInstance(): App {
     if (!Admin.app) {
-      const serviceAccount: ServiceAccount = require('/home/alexandrecr/devs/gorda/firebaseAccount.json')
       Admin.app = initializeApp({
-        credential: cert(serviceAccount),
+        credential: applicationDefault(),
         databaseURL: 'https://gorda-driver-default-rtdb.firebaseio.com',
       })
       const db = getDatabase(Admin.app)
-      if (process.env.NODE_ENV !== 'production') {
+      if (process.env.NODE_ENV == 'local') {
         db.useEmulator('localhost', 9000)
       }
     }
