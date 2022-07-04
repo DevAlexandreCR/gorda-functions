9b7662d537f38c60a224771b4da54ae7e8e52370
AlexandreCR <devalexandrecr@gmail.com>
2022-07-04 16:16:55 -0500

    support: update functions

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,10 @@
 import * as functions from 'firebase-functions'
 import {Express} from 'express'
-import * as cors from 'cors'
 import * as express from 'express'
 import {authRouter} from './routes/auth/controller'
 import * as services from './routes/services/controller'
 
 const api: Express = express()
-api.use(cors({origin: true}))
 api.use('/auth', authRouter)
 
 exports.api = functions.https.onRequest(api)
